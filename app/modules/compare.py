d4663750aa085cfe9c2e242ea910b31d003a21a5
= <martin.hjelmeland@ntnu.no>
2024-02-27 20:19:14 +0100

    Add option to discount energy prices

--- app/modules/compare.py
+++ app/modules/compare.py
@@ -4,9 +4,21 @@ import streamlit as st
 
 from app.modules.utils import get_valid_data_folders
 from empire.output_client.client import EmpireOutputClient
+from empire.core.config import read_config_file, EmpireConfiguration
 
 # active_results = [i for i in (Path.cwd() / "Results/1_node").glob("*")][-1]
 
+def undiscount_prices(df, empire_config):
+    
+    years_to_period_mapping = {
+        f"{2020 + i*empire_config.leap_years_investment}-{2020+empire_config.leap_years_investment+i*empire_config.leap_years_investment}": i
+        for i in range(empire_config.n_periods)
+    }
+    df.loc[:, "Price_EURperMWh"] = df.apply(
+        lambda x: x["Price_EURperMWh"]
+        * (1 + empire_config.discount_rate) ** (years_to_period_mapping[x["Period"]]),
+        axis=1,
+    )
 
 def compare(folders):
     st.title("Compare runs")
@@ -23,6 +35,9 @@ def compare(folders):
 
     ouput_client_1 = EmpireOutputClient(active_results_1 / "Output")
     ouput_client_2 = EmpireOutputClient(active_results_2 / "Output")
+
+    empire_config_1 = EmpireConfiguration.from_dict(config=read_config_file(active_results_1 / "Input/Xlsx/config.txt"))
+    empire_config_2 = EmpireConfiguration.from_dict(config=read_config_file(active_results_2 / "Input/Xlsx/config.txt"))
 
     df_op = ouput_client_1.get_node_operational_values()
     df_op["Case"] = "Case1"
@@ -47,7 +62,6 @@ def compare(folders):
     period_2 = col2.selectbox("Period2: ", df_op["Period"].unique())
     scenario_2 = col2.selectbox("Scenario2: ", df_op["Scenario"].unique())
 
-    
 
     df_2 = df_op.query(f"Node=='{node_2}' and Period=='{period_2}' and Scenario=='{scenario_2}'")
     
@@ -57,6 +71,14 @@ def compare(folders):
         .union(set(df_2.columns))
         .difference(set(["Node", "Period", "Scenario", "Season", "Case", "Hour"])),
     )
+    if value == "Price_EURperMWh":
+        discount_1 = col1.toggle("Discount prices:", value=True)
+        discount_2 = col2.toggle("Discount prices: ", value=True)
+        if not discount_1:
+            undiscount_prices(df_1, empire_config=empire_config_1)
+        if not discount_2:
+            undiscount_prices(df_2, empire_config=empire_config_2)
+    
     markers = st.toggle("Use markers")
     fig = px.line(
         pd.concat([df_1, df_2]),
